6e7c13637658236e8bbd3588cf54746b65071350
Guilherme Macedo <macedoguilherme96@gmail.com>
2020-04-27 16:23:49 -0300

    Exercicio 31º Incompleto

---
+++ exercicio31/src/entities/Valores.java
@@ -0,0 +1,32 @@
+package entities;
+
+import java.util.ArrayList;
+
+public class Valores {
+	
+	private Integer numero;
+	
+	Integer[] numeros = new Integer[3];
+
+	public Valores(Integer numero) {
+		super();
+		this.numero = numero;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public void odernacao() {
+		int aux = -9999;
+		for(int i = 0; i <= numeros.length; i++) {
+			
+		}
+	}
+	
+
+}
